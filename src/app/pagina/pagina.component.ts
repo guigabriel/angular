a56f9ca7a346ca91f62414f0dee76aedba4df057
Guilherme Gabriel <guilhermelisboa98@outlook.com>
2021-12-10 15:09:30 -0300

    primeiro commit

---
+++ src/app/pagina/pagina.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+import { catchError } from 'rxjs/operators';
+
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-pagina',
+  templateUrl: './pagina.component.html',
+  styleUrls: ['./pagina.component.scss']
+})
+export class PaginaComponent implements OnInit {
+
+  public apiGreeting = '';
+  public inputGet = '';
+
+  constructor(
+    private apiService: ApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.apiService.getHello().pipe(
+      catchError((err) => {
+        this.apiGreeting = 'Falha na comunicação com o servidor.';
+        return [];
+      })
+    ).subscribe((response) => {
+      if (response) {
+        this.apiGreeting = response.mensagem;
+      }
+    });
+  }
+  
+  resposta(){
+    console.log(this.inputGet)
+  }
+    
+  
+
+}
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  apiUrl = 'http://127.0.0.1:8000/api/teste'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getHello(): Observable<any> {
+    return this.http.get(environment.apiHost + '/hello');
+  }
+
+   public getData(): Observable<any> {
+    return this.http.get(environment.apiHost + '/dataAtual');
+
+}
